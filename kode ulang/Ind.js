4fb71d0da49f58859c0a51d936c57b2e70a789cf
Rizkyaebg <97835768+Rizkyaebg@users.noreply.github.com>
2022-01-17 05:57:18 +0700

    Create Ind.js

---
+++ kode ulang/Ind.js
@@ -0,0 +1,14 @@
+ekspor.donasi = () => { 
+return`*HAI KAK MAU BENERAN DONASI ATAU TIDAK KAK*
+*SEMOGA TIDAK DILIHAT DOANG*
+
+*BOT PEMBAYARAN*
+DANA: (0882-7926-8363)
+OVO: MELALUI QR
+PULSA: MELALUI QR AJA
+
+*TERIMA KASIH KAK*
+*CINTA KAMU😋*`
+}
+export.menu = (awalan, simbol) => { 
+kembali`======[ ]=====
